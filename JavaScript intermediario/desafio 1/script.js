8de93ab6f2bc29376c533cf844813c6770752455
jadsongouveia <jadsongouveia12@gmail.com>
2023-03-26 21:20:40 -0300

    novos aprendizados

---
+++ JavaScript intermediario/desafio 1/script.js
@@ -0,0 +1,11 @@
+
+
+let botaoTeste = document.querySelector('#botaoTeste')
+
+let count = 0
+
+function cliqueBotao(){
+    document.write(click = contador)
+}
+
+botaoTeste.addEventListener('click', cliqueBotao)
