905568ed3261888bbb92bd050a1f7fb85d1cd5b4
jadsongouveia <jadsongouveia12@gmail.com>
2023-03-27 22:04:48 -0300

    novos aprendizados

--- JavaScript intermediario/desafio 1/script.js
+++ JavaScript intermediario/desafio 1/script.js
@@ -1,11 +1,25 @@
 
 
-let botaoTeste = document.querySelector('#botaoTeste')
+let btn = document.querySelector('#btn')
+
+let texto = document.querySelector('#texto')
 
 let count = 0
 
-function cliqueBotao(){
-    document.write(click = contador)
-}
+btn.addEventListener("click", function(){
+    count++
+    texto.innerHTML = count
 
-botaoTeste.addEventListener('click', cliqueBotao)
+})
+
+
+let btn2 = document.querySelector('#btn2')
+
+let texto2 = document.querySelector('#texto2')
+
+btn2.addEventListener("click", function(){
+    count--
+    texto2.innerHTML = count
+})
+
+    
