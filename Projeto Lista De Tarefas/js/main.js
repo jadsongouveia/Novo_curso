a7e039ff106fbcaf0d278ea8ab670ea74d364b43
jadsongouveia <jadsongouveia12@gmail.com>
2023-03-28 21:17:03 -0300

    novos aprendizados

--- Projeto Lista De Tarefas/js/main.js
+++ Projeto Lista De Tarefas/js/main.js
@@ -3,23 +3,59 @@ const Main = {
 
   init: function() {
     this.cacheSelectors()
-    this.bindEvents
+    this.bindEvents()
     },
 
   cacheSelectors: function() {
-    this.checkButtons = document.querySelectorAll('.check')
+    this.$checkButtons = document.querySelectorAll('.check')
+    this.$inputTask = document.querySelector('#inputTask')
+    this.$list = document.querySelector('#list')
     },
 
   bindEvents: function() {
-    this.checkButtons.forEach(function(button){
-       button.onclick = function(){
-        alert('ok')
+    const self = this
+
+    this.$checkButtons.forEach(function(button){
+       button.onclick = self.Events.checkButton_click
+     })
+
+     this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+    },
+
+
+
+    Events: {
+      checkButton_click: function(e) {
+        const li = e.target.parentElement
+        const isDone = li.classList.contains('done')
+
+        if (!isDone) {
+          return li.classList.add('done')  
+        } 
+
+        li.classList.remove('done')
+      },
+
+      inputTask_keypress: function(e){
+       const key =e.key
+       const value = e.target.value
+
+       if (key === 'Enter') {
+        this.$list.innerHTML += `
+          <li>
+            <div class="check"></div>
+            <label class="task">
+              ${value}
+            </label>
+            <button class="remove"></button>
+          </li>
+        `
+
+        e.target.value = ''
        }
-      })
-            
+      }
     }
 
 }
-
 
 Main.init()
